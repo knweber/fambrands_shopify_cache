9044b00ffe8c2458c0d5248714981170f7914c14
Kris <kris.weber.1994@gmail.com>
2018-01-05 11:53:32 -0800

    Add migration for Orders

---
+++ db/migrate/20180105115119_create_orders.rb
@@ -0,0 +1,32 @@
+class CreateOrders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :orders do |t|
+      t.string :unique_order_number
+      t.string :email
+      t.string :phone
+      t.string :first_name
+      t.string :last_name
+      t.decimal :total_price
+      t.decimal :subtotal
+      t.decimal :total_discounts
+
+      t.string :sku
+      t.decimal :weight
+      t.string :weight_units
+      t.string :item_name
+
+      t.string :billing_customer_name
+      t.string :billing_address
+      t.string :billing_city
+      t.string :billing_zip
+      t.string :billing_state
+      t.string :billing_country
+
+      t.string :shipping_address
+      t.string :shipping_address2
+      t.string :shipping_city
+      t.string :shipping_zip
+      t.string :shipping_state
+      t.string :shipping_country
+    end
+  end
